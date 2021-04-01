441c40c9c1c2da710ef3339729b8e25cf7c75ab9
Tola Voeung <tolacambo@gmail.com>
2021-04-01 13:58:33 +0700

    Created CosyncJunction ReactNative version

---
+++ CosyncJunction/ReactNative/CosyncJunctionSample/components/DrawerNavigationRoutes.js
@@ -0,0 +1,70 @@
+ 
+//
+//  DrawerNavigatorRoutes.js
+//  CosyncStorageSample
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//  Created by Tola Voeung.
+//  Copyright © 2020 cosync. All rights reserved.
+//
+
+
+import React from 'react';
+
+//Import Navigators
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+//Import External Screens
+import ProfileScreen from '../screens/ProfileScreen'; 
+import CustomSidebarMenu from './CustomSidebarMenu';
+import NavigationDrawerHeader from './NavigationDrawerHeader';
+
+const FirstActivity_StackNavigator = createStackNavigator({
+  First: {
+    screen: ProfileScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Profile Screen',
+      headerLeft: () => <NavigationDrawerHeader navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#307ecc',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+ 
+
+const DrawerNavigatorRoutes = createDrawerNavigator(
+  {
+    ProfileScreen: {
+      screen: FirstActivity_StackNavigator,
+      navigationOptions: {
+        drawerLabel: 'Prifile Screen',
+      },
+    } 
+  },
+  {
+    contentComponent: CustomSidebarMenu,
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle',
+  }
+);
+export default DrawerNavigatorRoutes;
---
+++ CosyncJunction/ReactNative/CosyncJunctionSample/screens/ProfileScreen.js
@@ -0,0 +1,223 @@
+ 
+//
+//  ProfileScreen.js
+//  CosyncJunctionSample
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//  Created by Tola Voeung.
+//  Copyright © 2020 cosync. All rights reserved.
+//
+
+import React, { useState, useRef } from 'react'; 
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import Loader from '../components/Loader'; 
+import Configure from '../config/Config'; 
+import * as RealmLib from '../managers/RealmManager'; 
+
+const LoginScreen = props => {
+  
+  let [userEmail, setUserEmail] = useState('');
+  let [userPassword, setUserPassword] = useState('');
+  let [loading, setLoading] = useState(false);
+  let [errortext, setErrortext] = useState('');
+  const ref_input_pwd = useRef();
+  
+
+  global.appId = Configure.Realm.appId; 
+  AsyncStorage.setItem('appId', global.appId);  
+
+
+  const validateEmail = (text) => {
+   
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (reg.test(text) === false) return false;
+    else return true;
+  }
+
+  
+  const handleSubmitPress = () => {
+    setErrortext('');
+    if (!userEmail) {
+      alert('Please fill Email');
+      return;
+    }
+    if (!validateEmail(userEmail)) {
+      alert('Please fill a valid email');
+      return;
+    }
+
+    if (!userPassword) {
+      alert('Please fill Password');
+      return;
+    }
+    
+    AsyncStorage.setItem('user_email', userEmail);  
+    AsyncStorage.setItem('user_password', userPassword); 
+
+    setLoading(true);  
+
+    RealmLib.login(userEmail, userPassword).then(user => {
+     
+      AsyncStorage.setItem('user_id', user.id);
+      //props.navigation.navigate('AssetScreen'); 
+      props.navigation.navigate('DrawerNavigationRoutes'); 
+      
+    }).catch(err => {
+      setLoading(false);
+      setErrortext(err.message);
+    }) 
+     
+  };
+
+  return (
+    <View style={styles.mainBody}>
+      <Loader loading={loading} />
+      
+      <ScrollView keyboardShouldPersistTaps="handled">
+
+        <View style={{ marginTop: 100 }}>
+          <KeyboardAvoidingView enabled>
+            <View style={{ alignItems: 'center' }}>
+              <Image
+                source={require('../assets/cosynclogo.png')}
+                style={{ 
+                  height: 200,
+                  resizeMode: 'contain',
+                  margin: 30,
+                }}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={UserEmail => setUserEmail(UserEmail)}
+                //underlineColorAndroid="#4638ab"
+                placeholder="Enter Email" 
+                autoCapitalize="none"
+                keyboardType="email-address" 
+                returnKeyType="next" 
+                onSubmitEditing={() => ref_input_pwd.current.focus()}
+                blurOnSubmit={false}
+                
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={UserPassword => setUserPassword(UserPassword)}
+                //underlineColorAndroid="#4638ab"
+                placeholder="Enter Password" 
+                keyboardType="default" 
+                returnKeyType="go"
+                onSubmitEditing={() => Keyboard.dismiss, handleSubmitPress}
+                blurOnSubmit={false}
+                textContentType={'none'}
+                autoComplete= {'off'}
+                secureTextEntry={true}
+                ref={ref_input_pwd}
+              />
+            </View>
+            {errortext != '' ? (
+              <Text style={styles.errorTextStyle}> {errortext} </Text>
+            ) : null}
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              activeOpacity={0.5}
+              onPress={handleSubmitPress}>
+              <Text style={styles.buttonTextStyle}>LOGIN</Text>
+            </TouchableOpacity>
+
+            { <Text
+              style={styles.registerTextStyle}
+              onPress={() => props.navigation.navigate('RegisterScreen')}>
+              Create New Account
+            </Text>}
+
+          </KeyboardAvoidingView>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#4638ab',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonTextStyle: {
+    color: 'white',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: '#4638ab',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: '#4638ab',
+  },
+  registerTextStyle: {
+    color: '#4638ab',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
+});
