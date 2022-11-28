4c38d180e0dde783546437cd0b74ed1098ba7d0c
Tola Voeung <tolacambo@gmail.com>
2022-11-28 21:52:08 +0700

    added CosyncJWT Expo

---
+++ CosyncJWT/CosyncJWTExpo/components/AppNav.js
@@ -0,0 +1,52 @@
+
+import React, { useContext } from 'react'; 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RegisterScreen from '../screens/RegisterScreen';
+import SignupScreen from '../screens/SignupScreen';
+import LoginScreen from '../screens/LoginScreen';
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen'; 
+import HomeScreen from '../screens/HomeScreen';
+import { AuthContext } from '../context/AuthContext';
+
+const Stack = createNativeStackNavigator();
+
+
+export default function AppNav() {
+  
+    const { userTokenData } = useContext(AuthContext);
+
+    return (
+        <NavigationContainer>
+            {userTokenData ? 
+                <AppStack/> : 
+                <AuthStack/>
+            }
+        </NavigationContainer>
+
+    )
+}
+
+
+const AuthStack = () => {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        
+      </Stack.Navigator>
+    );
+  };
+  
+  
+  const AppStack = () => {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+        
+      </Stack.Navigator>
+    );
+  };
+  
---
+++ CosyncJWT/CosyncJWTExpo/context/AuthContext.js
@@ -0,0 +1,177 @@
+
+import React, {createContext, useState, useEffect} from "react"
+import Configure from '../config/Config';  
+import CosyncJWTReactNative from 'cosync-jwt-react-native';  
+import uuid from 'react-native-uuid';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+export const AuthContext = createContext();
+  
+
+export function AuthProvider({ children }) {
+
+    const [userTokenData, setUserTokenData] = useState() 
+    const [loginToken, setLoginToken] = useState() 
+    const [userData, setUserData] = useState() 
+    const [appData, setAppData] = useState()  
+    const [realmUser, setRealmUser] = useState() 
+    const cosyncJWT = new CosyncJWTReactNative(Configure.CosyncApp).getInstance();
+
+    useEffect(() => {
+        getApplication();
+    }, []);
+
+    async function getApplication() {
+        cosyncJWT.app.getApplication().then(result => {  
+            setAppData(result) 
+            console.log('AuthContext getApplication ', result);
+
+        }).catch(err => {
+          
+          alert(`Invalid Data: ${err.message}`)
+        })
+    }
+
+    async function loginAnonymous(){
+        
+        try {
+
+            let id =  uuid.v4();
+            let result = await cosyncJWT.login.loginAnonymous(`ANON_${id}`); 
+
+            if(result.jwt) loginJWT(result);
+
+            return Promise.resolve(result);
+
+        } catch (error) {
+            return Promise.reject(error);
+        }
+       
+    }
+
+    
+
+    async function login(userEmail, userPassword) { 
+        try {
+        
+            let result = await cosyncJWT.login.login(userEmail, userPassword)
+            if(result.jwt){
+                loginJWT(result);
+            }
+            else if (result['login-token']){
+                setLoginToken(result['login-token']);
+            }
+
+            return Promise.resolve(result);
+
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+    async function loginComplete(loginCode){
+        try {
+            let result = await cosyncJWT.login.loginComplete(loginToken, loginCode);
+            if(result.jwt){
+                loginJWT(result);
+            }
+
+            return Promise.resolve(result);
+
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+    
+    async function signup(userEmail, userPassword, metaData){
+        try {
+            let result = await cosyncJWT.signup.signup(userEmail, userPassword, metaData); 
+            if(result && result.jwt && appData.signupFlow == 'none'){ 
+                loginJWT(result)
+            }
+
+            return Promise.resolve(result);
+
+        } catch (error) {
+
+            return Promise.reject(error);
+        }
+        
+    }
+
+    async function signupComplete(userEmail, signupCode){
+        try {
+            let result = await cosyncJWT.signup.completeSignup(userEmail, signupCode);
+            if(result && result.jwt && appData.signupFlow == 'none'){ 
+                loginJWT(result)
+            }
+
+            return Promise.resolve(result);
+
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+    async function loginJWT(tokenData){
+
+        let user = await cosyncJWT.realmManager.login(tokenData.jwt, Configure.Realm.appId); 
+        setRealmUser(user)
+        console.log("get realm user ", user.id); 
+        let data = await cosyncJWT.profile.getUser();
+        setUserData(data);
+        console.log("get user data ", data); 
+
+        setUserTokenData(tokenData);
+        console.log("get user jwt ", tokenData);
+        
+    }
+
+    async function register(userEmail, userPassword, inviteCode, metaData){
+        try {
+            let result = await cosyncJWT.register.register(userEmail, userPassword, inviteCode, metaData);
+            if(result && result.jwt){ 
+                loginJWT(result)
+            }
+
+            return Promise.resolve(result);
+
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+
+    function logout() {
+        setUserData();
+        setUserTokenData();
+        cosyncJWT.realmManager.logout();
+    }
+
+
+    const value = { 
+        cosyncJWT,
+        login, 
+        logout, 
+        register,
+        signup,
+        signupComplete,
+        loginAnonymous,
+        loginComplete,
+        getApplication,
+        loginToken,
+        userData,
+        userTokenData,
+        appData,
+        realmUser
+      }
+
+      
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
